56ab14ce2f08189ad8c171f04d5146ac6048fd13
nishant6342 <www.rajsonu13@gmail.com>
2022-10-12 15:32:34 +0000

    RMX3360: Inherit proprietary apps repository

--- device.mk
+++ device.mk
@@ -44,3 +44,6 @@ $(call inherit-product, device/oplus/sm8350-common/common.mk)
 
 # Inherit from the proprietary files makefile.
 $(call inherit-product, vendor/oplus/RMX3360/RMX3360-vendor.mk)
+
+# Call proprietary apps
+$(call inherit-product-if-exists, packages/apps/prebuilt-apps/prebuilt-apps.mk)
